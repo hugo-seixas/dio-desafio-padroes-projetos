41e3668c99cfa6e5294e1d8f18549804d58d5197
Hugo Seixas <hugo_seixas@yahoo.com.br>
2023-04-08 18:04:13 -0300

    Primeiro commit

---
+++ lab-padroes-projeto-java/src/one/digitalinnovation/gof/singleton/SingletonLazy.java
@@ -0,0 +1,19 @@
+package one.digitalinnovation.gof.singleton;
+
+//Singleton preguiçoso
+
+public class SingletonLazy {
+	
+	private static SingletonLazy instancia;
+	
+	private SingletonLazy() {
+	}
+	
+	public static SingletonLazy getInstancia() {
+		if (instancia == null) {
+			instancia = new SingletonLazy();
+		}
+		return instancia;
+	}
+
+}
